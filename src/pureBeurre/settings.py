3252ff253a010e2494dc9cd1b5b438179e545236
jbbaillet <jbbaillet.dev@gmail.com>
2021-10-22 10:16:41 +0200

    Mettre une image de fond ( https://unsplash.com/photos/eqsEZNCm4-c ) sur la page d'accueil.

--- src/pureBeurre/settings.py
+++ src/pureBeurre/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['purebeurre1.herokuapp.com']
+ALLOWED_HOSTS = ['purebeurre1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
