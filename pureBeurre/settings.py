6104e37881953e2b73975d0ac8ca532f52489bc0
jbbaillet <jbbaillet.dev@gmail.com>
2021-11-06 14:45:19 +0100

    balise static

--- pureBeurre/settings.py
+++ pureBeurre/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'), 'homepage/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
