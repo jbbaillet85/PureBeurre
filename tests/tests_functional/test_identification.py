a483196c07c12e956a331d747a82fb940d7e3f17
jbbaillet <jbbaillet.dev@gmail.com>
2021-11-10 17:26:55 +0100

    correction du test identification

--- tests/tests_functional/test_identification.py
+++ tests/tests_functional/test_identification.py
@@ -23,7 +23,7 @@ class TestIdentification(StaticLiveServerTestCase):
         signup = self.browser.find_element_by_id("submit_register")
         signup.submit()
 
-        self.assertEqual(self.browser.find_element_by_tag_name('h1').text, "Créer son espace utilisateur")
+        self.assertEqual(self.browser.find_element_by_tag_name('h2').text, "Créer son espace utilisateur")
         self.assertEqual(self.browser.current_url, self.live_server_url + reverse("register"))
         # close the browser
         time.sleep(3)
